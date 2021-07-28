1d98646751b4b94d7099d34f1ab2727a85256797
Chunyu ZHAO <2604783z@gla.ac.uk>
2021-07-29 01:22:05 +0800

    add exercise 6

--- rango/views.py
+++ rango/views.py
@@ -7,10 +7,12 @@ from rango.models import Category, Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
 
     context_dict = {
         'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
-        'categories': category_list
+        'categories': category_list,
+        'pages': page_list
     }
 
     return render(request, 'rango/index.html', context=context_dict)
