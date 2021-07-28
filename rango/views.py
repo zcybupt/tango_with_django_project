2774927895c6807a08f08ad173c11bbd54655cfa
Chunyu ZHAO <2604783z@gla.ac.uk>
2021-07-29 00:54:41 +0800

    add category page

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
-from rango.models import Category
+from rango.models import Category, Page
 
 
 def index(request):
@@ -18,3 +18,18 @@ def index(request):
 
 def about(request):
     return render(request, 'rango/about.html')
+
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+
+        context_dict['category'] = category
+        context_dict['pages'] = Page.objects.filter(category=category)
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
