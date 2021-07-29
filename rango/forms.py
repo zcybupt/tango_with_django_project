4536f40693b8cee35dc7d43706e0c6baf365eb1f
Chunyu ZHAO <2604783z@gla.ac.uk>
2021-07-29 20:41:40 +0800

    reuse max_length

--- rango/forms.py
+++ rango/forms.py
@@ -3,7 +3,7 @@ from rango.models import Page, Category
 
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text='Please enter the category name.')
+    name = forms.CharField(max_length=Category.NAME_LENGTH, help_text='Please enter the category name.')
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -14,7 +14,7 @@ class CategoryForm(forms.ModelForm):
 
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128, help_text='Please enter the title of the page.')
+    title = forms.CharField(max_length=Page.TITLE_LENGTH, help_text='Please enter the title of the page.')
     url = forms.URLField(max_length=200, help_text='Please enter the URL of the page.')
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
--- rango/models.py
+++ rango/models.py
@@ -4,7 +4,9 @@ from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    NAME_LENGTH = 128
+
+    name = models.CharField(max_length=NAME_LENGTH, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
@@ -21,8 +23,10 @@ class Category(models.Model):
 
 
 class Page(models.Model):
+    TITLE_LENGTH = 128
+
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=TITLE_LENGTH)
     url = models.URLField()
     views = models.IntegerField(default=0)
 
