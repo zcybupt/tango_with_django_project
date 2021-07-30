d92c30db5c011e807dc8e6993d1be6534bdede8a
Chunyu ZHAO <2604783z@gla.ac.uk>
2021-07-30 22:58:17 +0800

    add cookie test

--- rango/views.py
+++ rango/views.py
@@ -18,10 +18,16 @@ def index(request):
         'pages': page_list
     }
 
+    request.session.set_test_cookie()
+
     return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print('TEST COOKIE WORKED!')
+        request.session.delete_test_cookie()
+
     return render(request, 'rango/about.html')
 
 
