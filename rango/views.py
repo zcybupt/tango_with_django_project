3a75c1f64ec9e1ed9ed41c11e4794195fb993159
Chunyu ZHAO <2604783z@gla.ac.uk>
2021-07-30 22:17:57 +0800

    add logout

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
@@ -139,3 +139,9 @@ def user_login(request):
 @login_required
 def restricted(request):
     return HttpResponse("Since you're logged in, you can see this text!")
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('rango:index'))
