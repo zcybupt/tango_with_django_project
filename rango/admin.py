0832f845e31404615909cda412a177f912f16839
Chunyu ZHAO <2604783z@gla.ac.uk>
2021-07-30 20:33:10 +0800

    add UserProfile to models

--- rango/admin.py
+++ rango/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 
 class PageAdmin(admin.ModelAdmin):
@@ -15,3 +15,4 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
